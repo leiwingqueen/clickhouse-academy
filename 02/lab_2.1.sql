0e7417e10b3b3c67972d32b2f33bd0a4ef2a910d
leiwingqueen <leiwingqueen@gmail.com>
2024-05-04 18:31:02 +0800

    finish lab 2.1

--- 02/lab_2.1.sql
+++ 02/lab_2.1.sql
@@ -17,3 +17,16 @@ insert into test.pypi select TIMESTAMP,COUNTRY_CODE,URL,PROJECT from s3('https:/
 
 # Write a query using the count() function that returns the top 100 downloaded projects (i.e. the count() of the PROJECT column).
 select count() as c,PROJECT from test.pypi group by PROJECT order by c desc limit 100
+
+
+# Re-run the query from Step 6 above that returned the top 100 downloaded projects, but this time filter the results by only downloads that occurred in April of 2023. (Hint: check the toStartOfMonth() or toDate() functions.)
+select count() as c,PROJECT from test.pypi where TIMESTAMP>=toDate('2023-04-01') group by PROJECT order by c desc limit 100
+
+# Write a query that only counts downloads of Python projects that start with "boto". (Hint: LIKE allows partial matches.)
+SELECT 
+    PROJECT,
+    count() AS c
+FROM test.pypi
+WHERE PROJECT LIKE 'boto%'
+GROUP BY PROJECT
+ORDER BY c DESC;
