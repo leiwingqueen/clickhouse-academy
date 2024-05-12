7deb1b7e47ed99099bccd2bc5e92c261538c5b64
leiwingqueen <leiwingqueen@gmail.com>
2024-05-12 17:44:26 +0800

    write query

--- 04/lab_5.1.sql
+++ 04/lab_5.1.sql
@@ -17,3 +17,14 @@ select addr1,addr2,street,town,price from uk_price_paid order by price desc limi
 
 select type,avg(price) as avg_price from uk_price_paid group by type limit 10
 
+select county,sum(price) from uk_price_paid where lowerUTF8(county) in ['avon','essex', 'devon', 'kent', 'cornwall'] and date>=toDate('2020-01-01') and date<=toDate('2020-12-31') group by county limit 10
+
+select date,avg(price) as avg_price from uk_price_paid where date>=toDate('2005-01-01') and date<=toDate('2010-12-31') group by date order by avg_price desc
+
+# How many properties were sold in Liverpool each day in 2020?
+select date,count() from uk_price_paid where town='LIVERPOOL' and date>=toDate('2020-01-01') and date<=toDate('2020-12-31') group by date order by date
+
+with(
+	select max(price) from uk_price_paid
+)as mx_price
+select max(price)/mx_price as p,town from uk_price_paid group by town order by p desc
